7e92219e8cf4cb3777af88b425ac875734fdf7e9
Ryan-Vandeneeckhout <exigoan131@gmail.com>
2023-04-14 23:39:27 -0400

    update theme

--- src/components/hooks/globalhooks/useRotateThemeDaily.js
+++ src/components/hooks/globalhooks/useRotateThemeDaily.js
@@ -1,9 +1,9 @@
 export const RotateThemeDaily = () => {
   var myDate = new Date();
   if (window.localStorage.getItem("dataTheme") === (null || undefined)) {
-    if (myDate.getDay() == 6 || myDate.getDay() == 0) alert("Weekend!");
+    if (myDate.getDay() === 6 || myDate.getDay() === 0) alert("Weekend!");
   } else {
-    if (myDate.getDay() == 6 || myDate.getDay() == 0) alert("Weekend!");
+    if (myDate.getDay() === 6 || myDate.getDay() === 0) alert("Weekend!");
     else {
       alert("Weekday!");
     }
